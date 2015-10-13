46b354b484f3954a02719d529a3b7e718060a7bc
haoyan_yang <haoyan_yang@DESKTOP-T2P97CB>
2015-10-13 15:07:44 +0800

    补充添加查询类别业务

---
+++ server4customer/src/com/bepotato/action/SearchType.java
@@ -0,0 +1,82 @@
+package com.bepotato.action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bepotato.common.Data;
+import com.bepotato.model.Service;
+import com.bepotato.model.ShopImpl;
+import com.bepotato.model.Type;
+import com.bepotato.model.TypeImpl;
+
+public class SearchType extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public SearchType() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		List list = new ArrayList();
+		Service service=new Service();
+		list=service.getAllType();
+		
+		request.getSession().setAttribute(Data.TYPES, list);
+		response.sendRedirect("../edittype.jsp");
+	
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
